b010eec20cec5e5fd6c8a51f3b41e8a1123a1bf8
ManManBricks <chika.callis@gmail.com>
2016-08-18 12:02:28 +0100

    Commiting test.js

--- tests.js
+++ tests.js
@@ -8,9 +8,34 @@ var Note = require('./lib/note.js');
 
 describe("Note creation works properly", function() {
     it("assigns author based on the parameter supplied in the constructor", function() {
-        note = new Note("Hello world", "Chidiebere")
-        assert(note.author == "Chidiebere")
+        note = new Note("ManMan bricks")
+        assert(note.author == "ManMan bricks")
     })
+	
+	it("create a new note for an author based on the note content passed to the create function for the current author", 
+	function() {
+		note = new Note("ManMan bricks")
+		note.create("This is my first note")
+		assert( note.get(0) == "This is my first note" )
+	})
+	
+	it("list all notes for an author using listNotes function", function() {
+		note = new Note("ManMan bricks")
+		note.create("This is my first note")
+		note.create("This is my second note")
+		note.create("This is my third note")
+		list = note.listNotes();
+		assert( list == "undefined" )
+	})
+		
+	it("list all notes for an author using listNotes function", function() {
+		note = new Note("ManMan bricks")
+		note.create("This is my first note")
+		note.create("This is my second note")
+		note.create("This is my third note")
+		list = note.listNotes();
+		assert( list == "undefined" )
+	})
 })
 
 describe("Notes application increments number of notes as notes are added", function() {
